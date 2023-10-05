18132c128d745712d055fe384a6c5ed268f58940
fabianradenta <13522105@mahasiswa.itb.ac.id>
2023-10-05 22:01:01 +0700

    update

--- src/RegresiLinearBerganda.java
+++ src/RegresiLinearBerganda.java
@@ -13,7 +13,7 @@ public class RegresiLinearBerganda {
             sampel = tempsampel;
         } else { // masukkan dari file
             Matrix tempsampel = new Matrix(0, 0);
-            tempsampel = InputOutput.readMatrixFromFile();
+            IO.pReadMatrix(tempsampel);
             System.out.println("Masukkan nilai yang mau ditaksir :");
             MetodeOBE.bacaArray(soal);
             sampel = tempsampel;
