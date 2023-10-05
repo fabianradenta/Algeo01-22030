0f7eb8b1f96fe0da65a8c064d3927880e66b97bb
fabianradenta <13522105@mahasiswa.itb.ac.id>
2023-10-05 21:41:04 +0700

    balik ke double

--- src/InterpolasiPolinom.java
+++ src/InterpolasiPolinom.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class InterpolasiPolinom {
@@ -24,9 +25,11 @@ public class InterpolasiPolinom {
         return newMatrix;
     }
 
-    public static String polinomInterpolation(Matrix m){
+    public static double polinomInterpolation(Matrix m){
         int i;
         double x;
+        double hasil, xPangkat;
+        final StringBuffer str = new StringBuffer();
         
         MetodeOBE.matriksElimGaussJordan(m);
         
@@ -35,16 +38,17 @@ public class InterpolasiPolinom {
             String negative = String.format("%.4f", -m.data[i][m.getColumns() - 1]);
             String positive = String.format("%.4f", m.data[i][m.getColumns() - 1]);
             if (i == 0) {
-                res += (m.data[i][m.getColumns() - 1] <= 0 ? " - " + negative : " + " + positive);
-            } else if (i == 1) {
-                res += (m.data[i][m.getColumns() - 1] <= 0 ? " - " + negative : " + " + positive) + "x";
-            } else if (i == m.getRows() - 1) {
-                res += (m.data[i][m.getColumns() - 1] <= 0 ? " - " + negative : " " + positive) + "x^" + i;
-            } else {
-                res += (m.data[i][m.getColumns() - 1] <= 0 ? " - " + negative : " + " + positive) + "x^" + i;
-            }
+                    res += (m.data[i][m.getColumns() - 1] <= 0 ? " - " + negative : " + " + positive);
+                } else if (i == 1) {
+                    res += (m.data[i][m.getColumns() - 1] <= 0 ? " - " + negative : " + " + positive) + "x";
+                } else if (i == m.getRows() - 1) {
+                    res += (m.data[i][m.getColumns() - 1] <= 0 ? " - " + negative : " " + positive) + "x^" + i;
+                } else {
+                    res += (m.data[i][m.getColumns() - 1] <= 0 ? " - " + negative : " + " + positive) + "x^" + i;
+                }
         }
         System.out.println(res);
+        m.displayMatrix();
         System.out.println();
         
         System.out.println("Taksir nilai fungsi");
@@ -57,8 +61,39 @@ public class InterpolasiPolinom {
         for (i = 0; i < m.getRows(); i++) {
             taksiran += m.data[i][m.getColumns() - 1] * Math.pow(x, i);
         }
-        String strTaksiran = "Taksiran nilai f(" + x + ") pada titik tersebut adalah " + taksiran;
-        scanElmt.close();
-        return strTaksiran;
+        
+        return taksiran;
     }
+
+    // public static void main(String[] args) {
+    //     Scanner scanner = new Scanner(System.in);
+
+    //     System.out.print("Masukkan jumlah sampel: ");
+    //     int n = scanner.nextInt();
+
+    //     // Membaca sampel dari keyboard
+    //     Matrix sampel = inputSampelKeyboard(n);
+
+    //     // Menghasilkan matriks interpolasi
+    //     Matrix interpolasiMatrix = matrixGenerator(sampel);
+
+    //     // Menghitung polinom interpolasi
+    //     double hasilInterpolasi = polinomInterpolation(interpolasiMatrix);
+
+    //     System.out.println("Hasil interpolasi: " + hasilInterpolasi);
+    // }
+
+    // public static Matrix inputSampelKeyboard(int n) {
+    //     Scanner scanner = new Scanner(System.in);
+    //     Matrix data = new Matrix(n,2); // Mengasumsikan setiap sampel terdiri dari dua nilai (x dan y)
+
+    //     System.out.println("Masukkan nilai x dan y untuk setiap sampel:");
+    //     for (int i = 0; i < n; i++) {
+    //         System.out.print("Sampel " + (i + 1) + ": ");
+    //         data.data[i][0] = scanner.nextDouble(); // Input nilai x
+    //         data.data[i][1] = scanner.nextDouble(); // Input nilai y
+    //     }
+
+    //     return data;
+    // }
 }
