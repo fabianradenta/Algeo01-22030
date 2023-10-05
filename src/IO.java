501b22e747ea1856b3071dcab0b2dd43c76d2fbc
fabianradenta <13522105@mahasiswa.itb.ac.id>
2023-10-05 20:56:58 +0700

    ini file pengganti inputOutput

--- src/IO.java
+++ src/IO.java
@@ -2,23 +2,11 @@ import java.util.*;
 import java.io.*;
 
 public class IO{
-    public static void pReadMatrixFromFile(Matrix m){
-        Scanner scanner = new Scanner(System.in);
-        String fileName = scanner.nextLine();
-        scanner.close();
-        if (readMatrixFromFile(fileName) != null){
-            m = readMatrixFromFile(fileName);
-        }
-        else {
-            m = null;
-        }
-    }
-
     public static Matrix readMatrixFromFile(String fileName) {
         try (Scanner fileScanner = new Scanner(new File(fileName))) {
+            // Determine the number of rows and columns in the file
             int rowcnt = 0;
             int colcnt = 0;
-            Matrix matrixData = new Matrix(0, 0);
 
             while (fileScanner.hasNextLine()) {
                 String[] temp = fileScanner.nextLine().split(" ");
@@ -26,26 +14,91 @@ public class IO{
                 colcnt = Math.max(colcnt, temp.length);
             }
 
-            matrixData.rows = rowcnt;
-            matrixData.columns = colcnt;
+            // Create a new matrix with the determined dimensions
+            double[][] matrixData = new double[rowcnt][colcnt];
 
+            // Reset the file scanner
             fileScanner.close();
+            Scanner fileScanner2 = new Scanner(new File(fileName));
 
-            try (Scanner matrixScanner = new Scanner(new File(fileName))) {
-                int i = 0;
-                while (matrixScanner.hasNextLine()) {
-                    String[] temp = matrixScanner.nextLine().split(" ");
-                    for (int j = 0; j < temp.length; j++) {
-                        matrixData.data[i][j] = Double.parseDouble(temp[j]);
-                    }
-                    i++;
+            // Read the matrix data from the file
+            int i = 0;
+            while (fileScanner2.hasNextLine()) {
+                String[] temp = fileScanner2.nextLine().split(" ");
+                for (int j = 0; j < temp.length; j++) {
+                    matrixData[i][j] = Double.parseDouble(temp[j]);
+                }
+                i++;
+            }
+
+            // Create a new Matrix object and return it
+            Matrix resMatr = new Matrix(rowcnt, colcnt);
+
+            for (int p=0; p<rowcnt; p++){
+                for (int q=0; q<colcnt; q++){
+                    resMatr.data[p][q] = matrixData[p][q];
                 }
             }
 
-            return matrixData;
+            return resMatr;
         } catch (FileNotFoundException e) {
-            System.out.println("File " + fileName + "tidak ditemukan");
+            System.out.println("File not found: " + fileName);
             return null;
         }
     }
+
+    public static void writeMatrixToFile(Matrix m, String fileName) {
+        try (FileWriter writer = new FileWriter(new File(fileName))) {
+            for (int i = 0; i < m.rows; i++) {
+                for (int j = 0; j < m.columns; j++) {
+                    writer.write(Double.toString(m.data[i][j]));
+                    if (j < m.columns - 1) {
+                        writer.write(" ");
+                    }
+                }
+                writer.write("\n");
+            }
+            System.out.println("Matrix data has been written to " + fileName);
+        } catch (IOException e) {
+            System.out.println("Error writing to file: " + e.getMessage());
+        }
+    }
+
+    public static void writeStringToFile(String msg, String filePath){
+        try {
+            FileWriter writer = new FileWriter(filePath);
+            writer.write(msg);
+            // Close the FileWriter to ensure that the data is flushed and the file is saved
+            writer.close();
+            System.out.println("String berhasil ditulis ke file.");
+        } catch (IOException e) {
+            System.err.println("An error occurred while writing to the file: " + e.getMessage());
+        }
+    }
+
+    public void pWriteMatrix(Matrix m){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Masukkan path file yang dituju\t: ");
+        String fileName = scanner.nextLine();
+        writeMatrixToFile(m, fileName);
+        scanner.close();
+    }
+    
+    public void pWriteString(String msg){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Masukkan path file yang dituju\t: ");
+        String fileName = scanner.nextLine();
+        writeStringToFile(msg, fileName);
+        scanner.close();
+    }
+
+    
+    
+    // public static void main(String[] args) {    
+    //     Matrix matr = new Matrix(3, 3);
+    //     matr.readMatrix();
+    //     writeMatrixToFile(matr, "tes.txt");
+    //     readMatrixFromFile("../test/input/SPLAxB1.txt").displayMatrix();;
+    // }
+    
 }
